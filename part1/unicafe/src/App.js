e4b071b4bc3cf812aff2e13ade324652902ae268
Edmund S <37881435+C91F37@users.noreply.github.com>
2021-06-26 18:17:17 -0700

    1.10: unicafe step5
    1.10: unicafe step5

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,8 +1,15 @@
 import React, { useState } from 'react'
 
+const Statistic = (props) => {
+  return (
+    <>
+    {props.text} {props.value} <br/>
+    </>
+  )
+}
+
 const Statistics = ({good, bad, neutral}) => {
   let all = good + bad + neutral
-  console.log(all)
   if (all === 0) {
     return (
       <div>
@@ -15,40 +22,37 @@ const Statistics = ({good, bad, neutral}) => {
     return (
       <div>
         <h2>statistics</h2>
-        good {good} <br/>
-        neutral {neutral} <br/>
-        bad {bad} <br/>
-        all {good+neutral+bad} <br/>
+        <Statistic text='good' value ={good}/>
+        <Statistic text='neutral' value ={neutral}/>
+        <Statistic text='bad' value ={bad}/>
+        all {all} <br/>
         average {(good-bad)/(good+neutral+bad)} <br/>
         positive {good * 100/(good+neutral+bad)} %
       </div>
     )
   }
-
 }
+
+const Button = (props) => {
+  return(
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const handleGoodClick = () => {
-    setGood(good + 1)
-  }
-  const handleNeutralClick = () => {
-    setNeutral(neutral + 1)
-  }
-  const handleBadClick = () => {
-    setBad(bad + 1)
-  }
-
-
   return (
     <div>
       <h2>give feedback</h2>
-      <button onClick={handleGoodClick}>Good</button>
-      <button onClick={handleNeutralClick}>Neutral</button>
-      <button onClick={handleBadClick}>Bad</button>
+      <Button handleClick={()=>setGood(good+1)} text='Good' />
+      <Button handleClick={()=>setNeutral(neutral+1)} text='Neutral' />
+      <Button handleClick={()=>setBad(bad+1)} text='Bad' />
       <Statistics good={good} bad={bad} neutral={neutral}/>
     </div>
   )
